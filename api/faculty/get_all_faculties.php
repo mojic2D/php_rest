25e3226c37b7680c7192eb4dcddd3bbd12648a92
Dragoljub Mojic <mojic2d@gmail.com>
2022-05-24 09:51:00 +0200

    cors6

--- api/faculty/get_all_faculties.php
+++ api/faculty/get_all_faculties.php
@@ -1,11 +1,34 @@
 <?php
 
-header('Access-Control-Allow-Origin: http://studentportalfrontend.s3-website-us-east-1.amazonaws.com');
-header ('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-// header('Access-Control-Allow-Headers: *');
-header ('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-header('Access-Control-Max-Age: 86400');
+// header('Access-Control-Allow-Origin: *');
+// header ('Content-Type: application/json');
+// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+// // header('Access-Control-Allow-Headers: *');
+// header ('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+// header('Access-Control-Max-Age: 86400');
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+    // you want to allow, and if so:
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        // may also be using PUT, PATCH, HEAD etc
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+echo "You have CORS!";
 
 
 include_once '../../config/database.php';
