97c288c8ed6bad28c190e2b4efc0d8e764b3cdff
Dragoljub Mojic <mojic2d@gmail.com>
2022-05-24 12:56:30 +0200

    cors final

--- api/file/get_all_files.php
+++ api/file/get_all_files.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 header ('Content-Type: application/json');
 
 include_once '../../config/database.php';
