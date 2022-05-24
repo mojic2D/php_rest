846f6bc8ba65e6339cc4371b7fb68d6b058a8c1a
Dragoljub Mojic <mojic2d@gmail.com>
2022-05-24 10:33:15 +0200

    cors9

--- api/user/login.php
+++ api/user/login.php
@@ -1,8 +1,8 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
 header ('Content-Type: application/json');
-header ('Access-Control-Allow-Methods: POST');
+// header ('Access-Control-Allow-Methods: POST');
 
 include_once '../../config/database.php';
 include_once '../../models/user.php';
